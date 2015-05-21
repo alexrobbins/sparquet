ef59efde87809027942916089cd5e45f3c3190c5
Alex Robbins <alexander.j.robbins@gmail.com>
2015-05-21 16:47:01 +0000

    Add github url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sparquet "0.1.0"
   :description "Easy integration for thrift defined parquet and Flambo."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/alexrobbins/sparquet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure             "1.6.0"]
