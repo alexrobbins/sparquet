ca9eb0daf388acff580ed949f94726c1c1c99a21
Alex Robbins <alexander.j.robbins@gmail.com>
2015-05-21 16:44:35 +0000

    0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sparquet "0.1.0-SNAPSHOT"
+(defproject sparquet "0.1.0"
   :description "Easy integration for thrift defined parquet and Flambo."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
